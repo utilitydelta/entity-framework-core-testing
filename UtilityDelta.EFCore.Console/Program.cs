145906afc4f825f9329d951710cb9552d6697ec3
Tyson Brown <tyson.brown@deswik.com>
2017-08-18 17:05:02 +1000

    test console

---
+++ UtilityDelta.EFCore.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using UtilityDelta.EFCore.Database;
+using UtilityDelta.EFCore.Entities;
+
+namespace UtilityDelta.EFCore.Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var dbContext = new FamilyContext())
+            {
+                dbContext.Database.Migrate();
+
+                dbContext.Grandparents.Add(new Grandparent()
+                {
+                    Name = "Geoff" + DateTime.Now.ToLongTimeString()
+                });
+
+                dbContext.SaveChanges();
+
+                System.Console.WriteLine(dbContext.Grandparents.OrderByDescending(x => x.Id).FirstOrDefault().Name);
+                System.Console.WriteLine(dbContext.Grandparents.Count());
+            }
+        }
+    }
+}
